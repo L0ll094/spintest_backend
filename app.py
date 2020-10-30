09a24e2dea83ed3ab9e0f44cec2f3e1ac76d87ab
L0ll094 <62265200+L0ll094@users.noreply.github.com>
2020-10-30 17:52:46 +0100

    Now A spintest object is generated when do_calculation() is run with info from all the forms

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-
+import datetime as DT
 import requests
+import SpinTest.SpinTestClass as spintestModule
+import SpinTest.test as importedModule #for testing purposes
 from flask import Flask, render_template, make_response,jsonify,request
 
 
@@ -7,18 +9,20 @@ app = Flask(__name__)
 
 a={'first':3,'second':4}
 
-#Preparing containers to store incoming data
+#Preparing containers to store incoming data. None of these should be 0, so if they're sent to python as 0 
+#we know something's wrong.
 #Fluid properties
 densityParticle=0
-densityfeed=0
-kinviscosity=0
+densityFeed=0
+kinViscosity=0
 
 #Equipment properties
 Rcentrifuge=0
-L1=0
-L2=0
 V1=0
 V2=0
+L1=0
+L2=0
+
 
 #Spintest Data
 Nstart1=0
@@ -27,7 +31,6 @@ Nstart3=0
 Nstart4=0
 
 spintime1=0
-spintime2=0
 spintime3=0
 spintime4=0
 
@@ -41,8 +44,41 @@ residualSolids2=0
 residualSolids3=0
 residualSolids4=0
 
-tempSpinTest=0
-neededQ=0
+
+
+
+
+def do_calculations():
+    #ensure spintimes are provided as number of minutes
+    
+    spintimes=[spintime1, spintime2, spintime3,spintime4]
+    Nstarts=[Nstart1,Nstart2,Nstart3,Nstart4]
+    Speeds=[speed1,speed2,speed3,speed4]
+    ResidualSolids=[residualSolids1,residualSolids2,residualSolids3,residualSolids4]
+    
+    spintimes_min=[]
+    refTime=DT.datetime(1900,1,1)
+    print("\n\n\n\n")
+    for time in spintimes:
+        temp=DT.datetime.strptime(time,'%M:%S')
+        time_in_minutes=(temp-refTime).total_seconds()/60
+        print("Type of each time_in_minutes")
+        print(type(time_in_minutes))
+        spintimes_min.append(time_in_minutes)
+        
+        
+    print("Before reformatting time array:")
+    print(spintimes)
+    print("After reformatting time array:")
+    print(spintimes_min)
+    #remember to add TempSpinTest and Needed Q to below call once they have been added to form.
+        
+        
+    spintest_object=spintestModule.SpinTest(spinTimes=spintimes_min,Nstart=Nstarts,speeds=Speeds,residualSol=ResidualSolids,densityfeed=densityFeed,densityparticle=densityParticle,kinviscosity=kinViscosity)
+    print("Got here!")
+    print(spintest_object)
+    return
+
 
 
 #Below code was copied from a solution on stack overflow to resole the issue of not having appropriate CORS headers
@@ -64,21 +100,25 @@ def _process_fluid_properties(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
     #Take the incoming data and turning it into a python recognized dictionary
     incomingData=request.get_json(force=True)
-    print(incomingData)
+   # print(incomingData)
+    _generate_results()
+   
 
     #We expect a certain order of incoming data, but try to avoid guessing the name right in case angular did something
     #The keys correspond to the names of the Formcontrols in angular
     save_fluid_properties(incomingData)
+
     return response
 
 def save_fluid_properties(incomingData):
     list_of_the_keys=list(incomingData.keys())
     global densityParticle
-    global densityfeed
-    global kinviscosity
+    global densityFeed
+    global kinViscosity
     densityParticle=incomingData[list_of_the_keys[0]]
-    densityfeed=incomingData[list_of_the_keys[1]]
-    kinviscosity=incomingData[list_of_the_keys[2]]
+    densityFeed=incomingData[list_of_the_keys[1]]
+    kinViscosity=incomingData[list_of_the_keys[2]]
+    
 
 
 
@@ -87,7 +127,7 @@ def _process_equipment_properties(response):
     response.headers.add("Access-Control-Allow-Origin", "*")
     #Take the incoming data and turning it into a python recognized dictionary
     incomingData=request.get_json(force=True)
-    print(incomingData)
+    #print(incomingData)
     #We expect a certain order of incoming data, but try to avoid guessing the name right in case angular did something
     #The keys correspond to the names of the Formcontrols in angular
     save_equipment_properties(incomingData)
@@ -97,16 +137,17 @@ def save_equipment_properties(incomingData):
     list_of_the_keys=list(incomingData.keys())
     
     global Rcentrifuge
-    global L1
-    global L2
     global V1
     global V2
+    global L1
+    global L2
 
     Rcentrifuge=incomingData[list_of_the_keys[0]]
-    L1=incomingData[list_of_the_keys[1]]
-    L2=incomingData[list_of_the_keys[2]]
-    V1=incomingData[list_of_the_keys[3]]
-    V2=incomingData[list_of_the_keys[4]]
+    
+    V1=incomingData[list_of_the_keys[1]]
+    V2=incomingData[list_of_the_keys[2]]
+    L1=incomingData[list_of_the_keys[3]]
+    L2=incomingData[list_of_the_keys[4]]
 
     return
 
@@ -145,58 +186,59 @@ def save_spintest_data(incomingData):
     global residualSolids3
     global residualSolids4
 
-    global tempSpinTest
-    global neededQ
+    global TempSpinTest
+    global NeededQ
     
     
-    Nstart1=incomingData[list_of_the_keys[0]]
-    Nstart2=incomingData[list_of_the_keys[1]]
-    Nstart3=incomingData[list_of_the_keys[2]]
-    Nstart4=incomingData[list_of_the_keys[3]]
+  
+    spintime1=incomingData[list_of_the_keys[0]]
+    print(spintime1)
+    print(type(spintime1))
+    spintime2=incomingData[list_of_the_keys[1]]
+    spintime3=incomingData[list_of_the_keys[2]]
+    spintime4=incomingData[list_of_the_keys[3]]
+    
+    Nstart1=incomingData[list_of_the_keys[4]]
+    print(Nstart1)
+    print(type(Nstart1))
+    Nstart2=incomingData[list_of_the_keys[5]]
+    Nstart3=incomingData[list_of_the_keys[6]]
+    Nstart4=incomingData[list_of_the_keys[7]]
 
-    spintime1=incomingData[list_of_the_keys[4]]
-    spintime2=incomingData[list_of_the_keys[5]]
-    spintime3=incomingData[list_of_the_keys[6]]
-    spintime4=incomingData[list_of_the_keys[7]]
 
     speed1=incomingData[list_of_the_keys[8]]
+    print(speed1)
+    print(type(speed1))
     speed2=incomingData[list_of_the_keys[9]]
     speed3=incomingData[list_of_the_keys[10]]
     speed4=incomingData[list_of_the_keys[11]]
 
     residualSolids1=incomingData[list_of_the_keys[12]]
+    print(residualSolids1)
+    print(type(residualSolids1))
     residualSolids2=incomingData[list_of_the_keys[13]]
     residualSolids3=incomingData[list_of_the_keys[14]]
     residualSolids4=incomingData[list_of_the_keys[15]]
 
-    tempSpinTest=incomingData[list_of_the_keys[16]]
-    neededQ=incomingData[list_of_the_keys[17]]
+    TempSpinTest=incomingData[list_of_the_keys[16]]
+    NeededQ=incomingData[list_of_the_keys[17]]
     return
 
 
-    
-    
 
-
+def _generate_results():
+    do_calculations()
+    
+    return
 
 
 
 #routes and what happens when you send a request to them
 @app.route('/')
 def hello_world():
-    return "hello world"
+    return "This is the backend server. You can send post requests to /send_fluid_properties,/send_equipment_properties and /send_spintest_data."
 
 
-
-
-#This is a get Endpoint, since it is accessed through a URL. 
-# #Because i send hidden payloads in the body when posting. 
-@app.route('/testpage')
-def resopond_to_get_request():
-    if request.method == "OPTIONS":
-        return _build_cors_preflight_response()
-    if request.method == "GET":
-        return _corsify_actual_response(jsonify(a))
 
 @app.route('/send_fluid_properties', methods = ['POST'])
 def respond_to_fluid_properties():
@@ -226,7 +268,9 @@ def respond_to_spintest_data():
         #Nope doesn't work we need it, but it is now just added to my other processing.
         return _process_spintest_data(jsonify(a))
     
-
+@app.route('/fetch_results',methods=['GET'])
+def respond_with_results():
+    return _generate_results()
 
 
 
---
+++ test2.py
@@ -0,0 +1,4 @@
+import SpinTest.test as importedModule
+
+fakeClass_object=importedModule.fakeClass()
+print(fakeClass_object.giveName())
